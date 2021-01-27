6f844996801649b19c2b792a7ad04fc706865e00
Seanbon0611 <seand0611@gmail.com>
2021-01-26 19:08:31 -0800

    added new domain to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -33,7 +33,15 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       credentials: true
   end
   allow do
-    origins 'whispering-beach-85385.herokuapp.com'
+    origins 'http://cangwencook.com'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+  end
+  allow do
+    origins 'http://www.cangwencook.com'
 
     resource '*',
       headers: :any,
