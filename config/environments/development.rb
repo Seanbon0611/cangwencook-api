7d0c1603da806371826e45a8baba8845213499c5
Seanbon0611 <seand0611@gmail.com>
2021-01-27 20:36:16 -0800

    reverting changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,14 +27,14 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :google
-  config.action_controller.default_url_options = {host: 'https://whispering-beach-85385.herokuapp.com'}
+  config.action_controller.default_url_options = {host: 'localhost'}
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
 
   config.action_mailer.delivery_method = :smtp
-  host='https://whispering-beach-85385.herokuapp.com'
+  host='localhost:3000'
   config.action_mailer.default_url_options = { host: host, protocol: 'http'}
 
   config.action_mailer.smtp_settings = {
