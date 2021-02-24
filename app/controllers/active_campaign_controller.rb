c27e22b25331dd3d1c03ed8b30f3ab626a4b303b
Seanbon0611 <seand0611@gmail.com>
2021-02-24 10:08:04 -0800

    added methods to create new contacts and retireve contacts with active campaign api

--- app/controllers/active_campaign_controller.rb
+++ app/controllers/active_campaign_controller.rb
@@ -1,19 +1,17 @@
 class ActiveCampaignController < ApplicationController
-  include HTTParty
-  base_uri Rails.application.credentials.activecampaign[:url]
-  @client = ::ActiveCampaign::Client.new(
-    api_url: ,
-    api_token: Rails.application.credentials.activecampaign[:key]
-  )
 
   def new_contact
-    ActiveCampaign.create_contact(
-      email: params[:email]
-    )
+    http = HTTParty.post("#{Rails.application.credentials.activecampaign[:url]}/api/3/contacts", headers: {"Api-Token" => "#{Rails.application.credentials.activecampaign[:key]}"}, body: {contact: {email: params[:email]}}.to_json)
+    render json: {response: http}
+  end
+
+  def all_contacts
+    http = HTTParty.get("#{Rails.application.credentials.activecampaign[:url]}/api/3/contacts", headers: {"Api-Token" => "#{Rails.application.credentials.activecampaign[:key]}"})
+    render json: {contacts: http}
   end
 
   private
   def active_campaign_params
-    params.permit(:id, :email, :first_name, :last_name, :phone)
+    params.permit(:id, :contact, :email, :first_name, :last_name, :phone)
   end
 end
