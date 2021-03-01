947224a5154ba8427ba8b14d2195e70fc8dda7d3
Seanbon0611 <seand0611@gmail.com>
2021-03-01 08:32:35 -0800

    added method to create new collaborate and feedback instances

--- app/controllers/collaborate_controller.rb
+++ app/controllers/collaborate_controller.rb
@@ -1,2 +1,15 @@
 class CollaborateController < ApplicationController
+  def create
+    collaborate = Collaborate.new(collab_params)
+    if collaborate.save
+      render json: {message: "Request Sent"}, status: 200
+    else
+      render json: {message: "Error Sending Request"}, status: 400
+    end
+  end
+
+  private
+  def collab_params
+    params.permit(:name, :email, :message)
+  end
 end
--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -1,2 +1,15 @@
 class FeedbackController < ApplicationController
+  def create
+    feedback = Feedback.new(feedback_params)
+    if feedback.save
+      render json: {message: "Feedback Sent"}, status: 200
+    else
+      render json: {message: "Error Sending Feedback"}, status: 400
+    end
+  end
+
+  private
+  def feedback_params
+    params.permit(:name, :email, :message)
+  end
 end
