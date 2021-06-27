b9c818c544cc844674badb6892634b8c7445f390
Seanbon0611 <seand0611@gmail.com>
2021-06-26 22:15:01 -0700

    updated feedback controller to fire off the mailer when a new instance is created

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -2,6 +2,7 @@ class FeedbackController < ApplicationController
   def create
     feedback = Feedback.new(feedback_params)
     if feedback.save
+      FeedbackMailer.submit_feedback(feedback).deliver_now
       render json: {message: "Feedback Sent"}, status: 200
     else
       render json: {message: "Error Sending Feedback"}, status: 400
